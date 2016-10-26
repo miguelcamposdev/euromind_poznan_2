1bc3a37f38eebeebcb53c0e8b6aef98bec93d80a
Miguel Campos <camposmiguel@gmail.com>
2016-10-26 16:03:22 +0200

    Database

--- 05_Database/02_CompleteNoteList/app/src/main/java/com/miguelcr/a02_completenotelist/MainActivity.java
+++ 05_Database/02_CompleteNoteList/app/src/main/java/com/miguelcr/a02_completenotelist/MainActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.miguelcr.a02_completenotelist.database.DaoSession;
+import com.miguelcr.a02_completenotelist.database.User;
+import com.miguelcr.a02_completenotelist.database.UserDao;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextEmail ,editTextPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPass = (EditText) findViewById(R.id.password);
     }
 
     public void goToSignUp(View view) {
@@ -18,4 +28,34 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
     }
+
+    public void doLogin(View view) {
+        String email = editTextEmail.getText().toString();
+        String pass = editTextPass.getText().toString();
+
+        if(email.isEmpty()) {
+            editTextEmail.setError("Write an email");
+        } else {
+            if (pass.isEmpty()) {
+                editTextPass.setError("Write a valid password");
+            } else {
+                DaoSession connection = DatabaseConnection.getConnection(this);
+                UserDao userManager = connection.getUserDao();
+
+                // Search with userManager if exist one User with this email and pass
+                // SELECT * FROM User WHERE email='John' AND password='1234'
+                User u = userManager.queryBuilder()
+                        .where(UserDao.Properties.Email.eq(email),
+                                UserDao.Properties.Password.eq(pass)
+                        ).unique();
+
+                if (u != null) {
+                    Toast.makeText(this, "Login ok", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(this, "Login wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+            }
+        }
 }
