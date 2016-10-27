696c0ed124e707a3e27b0a21f5e8c18ebf8011e5
Miguel Campos <camposmiguel@gmail.com>
2016-10-27 19:26:59 +0200

    DuckHunt

--- 06_Games/01_DuckHunt/app/src/main/java/com/miguelcr/a01_duckhunt/GameActivity.java
+++ 06_Games/01_DuckHunt/app/src/main/java/com/miguelcr/a01_duckhunt/GameActivity.java
@@ -1,11 +1,20 @@
 package com.miguelcr.a01_duckhunt;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.miguelcr.a01_duckhunt.database.DaoSession;
+import com.miguelcr.a01_duckhunt.database.DatabaseConnection;
+import com.miguelcr.a01_duckhunt.database.User;
+import com.miguelcr.a01_duckhunt.database.UserDao;
 
 import java.util.Random;
 
@@ -16,6 +25,7 @@ public class GameActivity extends AppCompatActivity {
     ImageView duck;
     int counter = 0;
     Random random;
+    boolean gameOver = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +47,78 @@ public class GameActivity extends AppCompatActivity {
         nick = extras.getString("nick");
 
         textViewNick.setText(nick);
+
+        startCountDownTimer();
+
+    }
+
+    private void startCountDownTimer() {
+        // Countdown
+        new CountDownTimer(10000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                textViewTimer.setText(millisUntilFinished / 1000+"s");
+            }
+
+            public void onFinish() {
+                textViewTimer.setText("game over!");
+                gameOver = true;
+                showGameDialog();
+            }
+        }.start();
+    }
+
+    private void showGameDialog() {
+        // Save the new score
+        DaoSession connection = DatabaseConnection.getConnection(this);
+        UserDao userManager = connection.getUserDao();
+
+        // Search the user in the database
+        User u = userManager.load(id);
+
+        if(u.getScore()<counter) {
+            u.setScore(counter);
+            userManager.update(u);
+        }
+
+        // Show the dialog to Restart game or Show Ranking
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // Add the buttons
+        builder.setPositiveButton("Show Ranking", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                Intent i = new Intent(this, RankingUserActivity.class);
+                startActivity(i);
+            }
+        });
+        builder.setNegativeButton("Play again", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+
+            }
+        });
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
     }
 
     public void duckClicked(View view) {
 
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int maxHeight = displaymetrics.heightPixels - duck.getHeight();
-        int maxWidth = displaymetrics.widthPixels - duck.getWidth();
+        if(!gameOver) {
+            DisplayMetrics displaymetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+            int maxHeight = displaymetrics.heightPixels - duck.getHeight();
+            int maxWidth = displaymetrics.widthPixels - duck.getWidth();
 
-        int xPosition = random.nextInt(maxWidth - 0 + 1) + 0;
-        int yPosition = random.nextInt(maxHeight - 0 + 1) + 0;
+            int xPosition = random.nextInt(maxWidth - 0 + 1) + 0;
+            int yPosition = random.nextInt(maxHeight - 0 + 1) + 0;
 
-        duck.setX(xPosition);
-        duck.setY(yPosition);
+            duck.setX(xPosition);
+            duck.setY(yPosition);
 
-        counter++;
-        textViewDuck.setText(counter+" ducks");
+            counter++;
+            textViewDuck.setText(String.valueOf(counter));
+        }
     }
 }
