9fcf115b051e805ada369f107110089592ddf561
Miguel Campos <camposmiguel@gmail.com>
2016-10-28 17:18:33 +0200

    DuckHunt

--- 07_Server/01_DuckHunt/app/src/main/java/com/miguelcr/a01_duckhunt/MainActivity.java
+++ 07_Server/01_DuckHunt/app/src/main/java/com/miguelcr/a01_duckhunt/MainActivity.java
@@ -2,11 +2,13 @@ package com.miguelcr.a01_duckhunt;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.miguelcr.a01_duckhunt.database.DaoSession;
 import com.miguelcr.a01_duckhunt.database.DatabaseConnection;
@@ -27,34 +29,10 @@ public class MainActivity extends AppCompatActivity {
     public void startGame(View view) {
         String nick = editTextNick.getText().toString();
 
-        if(nick.isEmpty()) {
+        if (nick.isEmpty()) {
             editTextNick.setError("Write a nick name");
         } else {
-            DaoSession connection = DatabaseConnection.getConnection(this);
-            UserDao userManager = connection.getUserDao();
 
-            // Search id exist one user where nick="..."
-            // SELECT * FROM User WHERE nick='$nick'
-            User u = userManager.queryBuilder()
-                    .where(UserDao.Properties.Nick.eq(nick))
-                    .unique();
-
-            Intent i = new Intent(this,GameActivity.class);
-
-            if(u==null) {
-                u = new User();
-                u.setNick(nick);
-                u.setScore(0);
-                long newId = userManager.insert(u);
-
-                i.putExtra("userId",newId);
-                i.putExtra("nick",nick);
-            } else {
-                i.putExtra("userId",u.getId());
-                i.putExtra("nick",u.getNick());
-            }
-
-            startActivity(i);
         }
 
     }
@@ -62,5 +40,27 @@ public class MainActivity extends AppCompatActivity {
     public void showRanking(View view) {
         Intent i = new Intent(this, RankingUserActivity.class);
         startActivity(i);
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String nickname = params[0];
+            String urlRequestLogin = "http://rest.miguelcr.com/killduck/register?nickname=" + nickname;
+            String result = Util.getResultadoUrl(urlRequestLogin);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result == null) {
+                Toast.makeText(MainActivity.this, "Try again", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(i);
+            }
+        }
+
     }
 }
